3efd98ef2337b2a4467514776addeff7deb82709
Matteo <matteo@agence-88.fr>
2024-03-13 21:51:39 +0100

    GET Customer filter by owner

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,43 @@
+<?php
+// api/src/Doctrine/CurrentUserExtension.php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\Customer;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
+
+final readonly class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+
+    public function __construct(
+        private Security $security,
+    )
+    {
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Customer::class !== $resourceClass || null === $user = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.owner = :current_user', $rootAlias));
+        $queryBuilder->setParameter('current_user', $user->getId());
+    }
+}
